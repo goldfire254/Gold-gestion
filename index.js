a2a09380c645729f257e8dbc78d822fe2a360c6a
goldfire254 <111021859+goldfire254@users.noreply.github.com>
2022-10-14 00:06:06 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents})
 const loadCommands = require('./loaders/loadcommands')
 const loadEvents = require('./loaders/loadEvents')
-let s3 = new aws.S3({
-    accessKeyId: process.env.S3_KEY,
-  });
+
 
 bot.commands = new Discord.Collection()
 
 loadCommands(bot)
 loadEvents(bot)         
 
-bot.login(s3);
+bot.login(process.env.TOKEN);
